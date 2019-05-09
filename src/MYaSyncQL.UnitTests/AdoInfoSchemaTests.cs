4e2d5e6a2be9c04d41c13b442d763cb4e7eccd84
penCsharpener <matthiasmueller@protonmail.com>
2019-05-09 23:45:40 +0200

    add unit test project

---
+++ src/MYaSyncQL.UnitTests/AdoInfoSchemaTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace MYaSyncQL.UnitTests {
+    public class AdoInfoSchemaTests {
+
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CleanUp() {
+
+        }
+    }
+}
