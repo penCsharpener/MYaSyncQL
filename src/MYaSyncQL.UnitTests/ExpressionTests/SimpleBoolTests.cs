fcc0b5c77265eddd8d5f89f7b3b3a51531b0a983
penCsharpener <matthiasmueller@protonmail.com>
2019-05-10 19:32:14 +0200

    add project WhereExpressions

---
+++ src/MYaSyncQL.UnitTests/ExpressionTests/SimpleBoolTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MYaSyncQL.UnitTests.ExpressionTests {
+    public class SimpleBoolTests {
+
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CleanUp() {
+
+        }
+    }
+}
---
+++ src/MYaSyncQL.WhereExpressions/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace MYaSyncQL.WhereExpressions {
+    public class Class1 {
+    }
+}
